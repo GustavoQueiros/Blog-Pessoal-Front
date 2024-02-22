71a2ed10b3e78e8280935c4524242cbb8837fda0
GustavoQueiros <gustavo.dev47@gmail.com>
2024-02-22 15:32:00 -0300

    Crud de Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import './App.css';
 
 import Navbar from './components/navbar/NavBar';
@@ -11,6 +11,10 @@ import { AuthProvider } from './contexts/AuthContexts';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormularioTema from './components/temas/formularioTema/FormularioTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+
+import ListaPostagens from './components/postagens/cardPostagem/listaPostagens/ListaPostagens';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 
 function App() {
@@ -29,6 +33,10 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
